09957c1594c3ea4c193f87dc4495867008130e2b
mark mayoral <markmayoral@marks-MacBook-Pro.local>
2016-06-28 13:33:44 -0400

    added files

--- reach/ViewController.swift
+++ reach/ViewController.swift
@@ -10,8 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myactivityindicator: UIActivityIndicatorView!
+    
+    @IBOutlet weak var welcomelabel: UILabel!
+    
+    @IBOutlet weak var loginbutton: UIButton!
+    @IBOutlet weak var registerbutton: UIButton!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "light-blue-backgrounds-2.jpg")!)
+        
+    
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +33,38 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewDidAppear(animated: Bool) {
+    }
+    
+    
+    @IBAction func registerbuttontapped(sender: AnyObject) {
+        
+        self.myactivityindicator.startAnimating();
+        self.myactivityindicator.hidden = false;
+        welcomelabel.hidden = false
+        loginbutton.hidden = true
+        registerbutton.hidden = true
+        self.myactivityindicator.center = self.view.center;
+        self.performSegueWithIdentifier("registerkeytappedhomepage", sender: self)
+       
+    }
+    
+    @IBAction func loginbuttontapped(sender: AnyObject) {
+        self.myactivityindicator.startAnimating();
+        self.myactivityindicator.hidden = false;
+        
+    
+    
+        welcomelabel.hidden = false
+        loginbutton.hidden = true
+        registerbutton.hidden = true
+        self.myactivityindicator.center = self.view.center;
+    
+    
 
-
+        self.performSegueWithIdentifier("hometologin", sender: self)    }
+    
 }
+
 
---
+++ reach/loginpageViewController.swift
@@ -0,0 +1,36 @@
+//
+//  loginpageViewController.swift
+//  reach
+//
+//  Created by mark mayoral on 6/18/16.
+//  Copyright © 2016 MDIndustries. All rights reserved.
+//
+
+import UIKit
+
+class loginpageViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
